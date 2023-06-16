edc501ba657b77d1288592ade4f5458a3ba64f5a
pratik2820 <130150715+pratik2820@users.noreply.github.com>
2023-06-16 17:50:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+// const counter = document.getElementById("counter");
+// const innerCounter = counter.innerText;
+let count = 0;
+function increment(){
+	count++;
+	document.getElementById("counter").innerText = count;
+}
